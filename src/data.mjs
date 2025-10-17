a3d85815c3751edcf3e5f09395373d64b221f566
Robbendebiene <robbendebiene@mailbox.org>
2025-10-17 20:26:53 +0200

    Add small command line tool to fetch GitHub repos the user committed to

---
+++ src/data.mjs
@@ -0,0 +1,88 @@
+import { graphql } from "@octokit/graphql";
+
+/**
+ * Using this service requires a valid GitHub auth token.
+ * The token does not need any additional permissions.
+ * See: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+ */
+export default class GitHubDataService {
+  #graphqlWithAuth;
+
+  constructor(accessToken) {
+    if (!accessToken) throw('You must provide a GitHub access token.');
+    this.#graphqlWithAuth = graphql.defaults({
+      headers: {
+        authorization: `token ${accessToken}`,
+      }
+    });
+  }
+
+  /**
+   * Query the years in which the user contributed.
+   */
+  async getContributionYears(year) {
+    const {
+      viewer: {
+        contributionsCollection: {
+          contributionYears
+        }
+      }
+    } = await this.#graphqlWithAuth(
+      `
+        query {
+          viewer {
+            login
+            contributionsCollection {
+              contributionYears
+            }
+          }
+        }
+      `
+    );
+    return contributionYears;
+  }
+
+  /**
+   * Query all repositories the user committed to from a given date limited by one year.
+   */
+  async getContributionsOfYear(year) {
+    const date = new Date(Date.UTC(year));
+    const {
+      viewer: {
+        login: user,
+        contributionsCollection: {
+          commitContributionsByRepository
+        }
+      }
+    } = await this.#graphqlWithAuth(
+      `
+        query ($from: DateTime!) {
+          viewer {
+            login
+            contributionsCollection(from: $from) {
+              commitContributionsByRepository(maxRepositories: 100) {
+                repository {
+                  nameWithOwner
+                  url
+                  viewerPermission
+                  stargazerCount
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        'from': date.toISOString(),
+      }
+    );
+
+    return commitContributionsByRepository.map((re) => ({
+      fullName: re.repository.nameWithOwner,
+      permission: re.repository.viewerPermission,
+      url: re.repository.url,
+      stars: re.repository.stargazerCount,
+      commitsURL: `https://github.com/${re.repository.nameWithOwner}/commits?author=${user}`
+    }));
+  }
+}
---
+++ src/main.mjs
@@ -0,0 +1,52 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import Handlebars from "handlebars";
+import minimist from 'minimist';
+import GitHubDataService from "./data.mjs";
+
+// RUN VIA: node src/main.mjs --templateFile pth/to/file.hbs --accessToken XXX
+
+var argv = minimist(process.argv.slice(2));
+
+const filePath = argv["templateFile"];
+const dataService = new GitHubDataService( argv["accessToken"]);
+
+
+// The GraphQL API is limited to return only data for 1 year.
+// Thus we first query the years in which the user was active and then make multiple queries per year.
+// The alternative `repositoriesContributedTo` does unfortunately not return all repositories.
+
+console.log('QUERY: years');
+
+const years = await dataService.getContributionYears();
+
+console.log('RESULT:', years);
+
+// deduplicate results from the GraphQL API
+const repoMap = new Map();
+
+for (const year of years) {
+  console.log(`QUERY: year ${year}`);
+  const results = await dataService.getContributionsOfYear(year);
+  console.log('RESULT:', results);
+  for (const result of results) {
+    // filter to only include external repos
+    if (result.permission === 'READ') {
+      repoMap.set(result.url, result);
+    }
+  }
+}
+
+// sort by stars
+const externalRepositories = Array.from(repoMap.values())
+  .sort((a, b) => b.stars - a.stars);
+
+console.log('RESULT:', externalRepositories);
+
+// Read template file and replace occurrences of "externalRepositories".
+
+const templateContents = readFileSync(filePath, 'utf8');
+const template = Handlebars.compile(templateContents);
+const substitutedContents = template({
+  externalRepositories
+});
+writeFileSync('README.md', substitutedContents, )
